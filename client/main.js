9f51f83bd9b5e1a1ee1195cb3e92e5a4a67be355
Levin Dixon <levindixon@gmail.com>
2014-11-08 15:10:14 -0800

    Stripe checkout init

--- client/main.js
+++ client/main.js
@@ -20,6 +20,24 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.payment.events({
+    'click button': function(e) {
+        e.preventDefault();
+        StripeCheckout.open({
+            key: 'pk_test_anR5YIoK80nL7qqTzkcCEyAv',
+            amount: 5000,
+            name: 'The Store',
+            description: 'A whole bag of awesome ($50.00)',
+            panelLabel: 'Pay Now',
+            token: function(res) {
+                // Do something with res.id
+                // Store it in Mongo and/or create a charge on the server-side
+                console.info(res);
+            }
+        });
+    }
+  });
+
   Template.body.helpers({
     users: function() {
       return Users.find({});
