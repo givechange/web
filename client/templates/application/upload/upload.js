7e8f56d7ab227ca4e70265b30c489eebaeb0bc04
shaunwong <shaun.kirk.wong@gmail.com>
2014-11-08 18:49:06 -0800

    uploader partial

--- client/templates/application/upload/upload.js
+++ client/templates/application/upload/upload.js
@@ -1,12 +1,18 @@
 Template.upload.events({
   'click button': function(e) {
-  	console.log(qrScanner)
-    var msg = qrScanner.message();
-
-    if (_.isUndefined(msg)) {
-        return;
+    console.log(this)
+    if (this._url) {
+      qrcode.callback = function (data) {
+        console.log('doSomething()');
+      };
+      console.log(this)
+      qrcode.decode(this._url);
     }
-
-    console.log(msg);
-  }
+  },
+  'change #camera-input': function(e) {
+    if (e.target.files.length == 1 && e.target.files[0].type.indexOf("image/") == 0) {
+      this._url = URL.createObjectURL(e.target.files[0]);
+      $("#yourimage").attr("src", this._url);
+    }
+  },
 });
