6b8652ca0ea8f464c1916a860bbb2377f48e8d78
Tim Greiser <tgreiser@gmail.com>
2014-11-08 18:33:53 -0800

    donation page in progress

--- client/templates/payment/payment.js
+++ client/templates/payment/payment.js
@@ -1,17 +1,25 @@
 Template.payment.events({
     'click button': function(e) {
         e.preventDefault();
-        console.log(e);
+        var amt = $(e.target).attr('x-amt');
+        var pennies = amt * 100;
         StripeCheckout.open({
             key: 'pk_test_anR5YIoK80nL7qqTzkcCEyAv',
-            amount: 5000,
-            name: 'The Store',
-            description: 'A whole bag of awesome ($50.00)',
-            panelLabel: 'Pay Now',
+            amount: pennies,
+            name: 'Give Change',
+            description: 'Donation ($' + amt + ')',
+            panelLabel: 'Give Now',
             token: function(res) {
                 // Do something with res.id
                 // Store it in Mongo and/or create a charge on the server-side
                 console.info(res);
+
+                Meteor.call('donationInsert', donation, function(error, result) {
+                  if (error) return throwError(error.reason);
+                  console.log("Saved donation: " + result);
+  
+                  Router.go('/thank_you');
+                });
             }
         });
     }
