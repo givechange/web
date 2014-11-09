926109da9e0b4d83a911353874ebc544866337e0
Tim Greiser <tgreiser@gmail.com>
2014-11-08 23:06:23 -0800

    qr generate - bugfix - defer loading until data is ready, for real this time?

--- client/templates/users/user_item.js
+++ client/templates/users/user_item.js
@@ -3,19 +3,21 @@ Template.userItem.helpers({
     return this._id;
   },
   url: function() {
+    if (this._id == undefined) return "";
     var url = _getUrl(this._id);
     return url;
+  },
+  qr: function() {
+    Meteor.defer(function() {
+      var url = $('#donatelink').attr('href');
+      if (url == "") return;
+      console.log(url);
+      var qrcodesvg   = new Qrcodesvg( url, "qrcode", 250);
+      qrcodesvg.draw();
+    });
+
   }
 });
-
-Template.userItem.rendered = function() {
-  if (this.data != undefined) {
-    console.log(this);
-    var url = _getUrl(this.data._id);
-    var qrcodesvg   = new Qrcodesvg( url, "qrcode", 250);
-    qrcodesvg.draw();
-  }
-}
 
 function _getUrl(id) {
   return "http://givechange.meteor.com/donate/" + id;
