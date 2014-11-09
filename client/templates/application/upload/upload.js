fd54e6d498152bd81d8c9c77cdc4340e032dd7d4
Levin Dixon <levindixon@gmail.com>
2014-11-08 20:57:44 -0800

    qr decoder fix

--- client/templates/application/upload/upload.js
+++ client/templates/application/upload/upload.js
@@ -1,18 +1,14 @@
 Template.upload.events({
-  'click button': function(e) {
-    console.log(this)
-    if (this._url) {
-      qrcode.callback = function (data) {
-        console.log('doSomething()');
-      };
-      console.log(this)
-      qrcode.decode(this._url);
-    }
-  },
   'change #camera-input': function(e) {
     if (e.target.files.length == 1 && e.target.files[0].type.indexOf("image/") == 0) {
       this._url = URL.createObjectURL(e.target.files[0]);
       $("#yourimage").attr("src", this._url);
     }
-  },
+    if (this._url) {
+      qrcode.callback = function (data) {
+        $("#result").text(data);
+      };
+      qrcode.decode(this._url);
+    }
+  }
 });
